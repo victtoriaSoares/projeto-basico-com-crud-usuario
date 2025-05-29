904d347999f89507afa7a207f20eb23975730974
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-05-28 21:03:37 -0400

    aula 28/05/2025

--- src/controllers/usuario/criar-usuario.ts
+++ src/controllers/usuario/criar-usuario.ts
@@ -1,10 +1,42 @@
 import { HttpRequest, HttpResponse } from '../../interfaces';
 import User from '../../models/user-model';
 import bcrypt from 'bcrypt';
+import validator from 'validator';
 class CriarUsuarioController {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { nome, email, senha } = httpRequest.body;
+
+      // Verifica se todos os campos obrigatórios foram preenchidos
+      if (!nome || !email || !senha) {
+        return {
+          statusCode: 400,
+          body: { error: 'Todos os campos são obrigatórios' },
+        };
+      }
+      // Verifica se o nome tem pelo menos 3 caracteres
+      if (nome.length < 3) {
+        return {
+          statusCode: 400,
+          body: { error: 'O nome deve ter pelo menos 3 caracteres' },
+        };
+      }
+
+      // Validação dos dados de entrada
+      if(validator.isEmail(email) === false) {
+        return {
+          statusCode: 400,
+          body: { error: 'Email inválido' },
+        };
+      }
+      const user = await User.findOne({ where: { email } });
+
+      if (user) {
+        return {
+          statusCode: 400,
+          body: { error: 'Email já cadastrado' },
+        };
+      }
 
       const salt = 10;
 
